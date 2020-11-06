4e68f3ddc37a350e66e8db40004ad6ddbbbf32a7
Ralph <ralphuitnederland@gmail.com>
2020-11-06 16:30:15 +0100

    fish theming

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -1,4 +1,15 @@
+# Custom colors
+set -xg fish_color_command        brgreen
+set -xg fish_color_quote          yellow
+set -xg fish_color_param          white
+set -xg fish_color_error          brred
+set -xg fish_color_autosuggestion brblack
+set -xg fish_color_comment        brblack
+
 # Custom bobthefish theme settings
-set -xg theme_powerline_fonts no
-set -xg theme_display_date no
+set -xg theme_powerline_fonts      no
+set -xg theme_display_date         no
 set -xg theme_display_cmd_duration no
+set -xg theme_display_user         no
+set -xg theme_display_hostname     ssh
+set -xg theme_newline_cursor       no
